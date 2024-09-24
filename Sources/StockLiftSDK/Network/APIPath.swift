9e01ce12d658ec725355adb62cb46dd33aa17bd8
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-09-24 12:11:11 +0700

    config endpoint class

---
+++ Sources/StockLiftSDK/Network/APIPath.swift
@@ -0,0 +1,14 @@
+//
+//  APIPath.swift
+//  StockLiftSDK
+//
+//  Created by Christopher Hicks on 9/24/24.
+//
+
+import Foundation
+
+public enum APIPath {
+    public enum v1 {
+        public static let stock = "/v1/stock"
+    }
+}
---
+++ Sources/StockLiftSDK/Network/Endpoint/Endpoint.swift
@@ -0,0 +1,77 @@
+//
+//  Endpoint.swift
+//  Stocklift
+//
+//  Created by Christopher Hicks on 7/20/22.
+//
+//
+
+import Foundation
+
+fileprivate enum AppEnv {
+    case prod, dev
+    
+    var name: String {
+        switch self {
+        case .prod:
+            return "Production"
+        case .dev:
+            return "Development"
+        }
+    }
+}
+
+public struct Endpoint {
+    public var path: String
+    public var queryItems: [URLQueryItem]
+    
+    public init(path: String, queryItems: [URLQueryItem] = []) {
+        self.path = path
+        self.queryItems = queryItems
+    }
+    
+    private var appEnv: AppEnv {
+        self.getEnvironment()
+    }
+}
+
+extension Endpoint {
+    var url: URL {
+        var components = URLComponents()
+        switch appEnv {
+        case .prod:
+            components.scheme = "https"
+            components.host = "sdk-stocklift-9a545489079b.herokuapp.com"
+            components.port = nil
+            
+        case .dev:
+            components.scheme = "https"
+            components.host = "sdk-dev-79b2be5acd71.herokuapp.com"
+            components.port = nil
+    
+        }
+        
+        components.path = "/" + path
+        components.queryItems = queryItems
+        guard let url = components.url else {
+            preconditionFailure("🌎 Invalid URL Components: \(components)")
+        }
+
+        print("🐞URL DEBUG: \(url)")
+        print("🌎 -> App is running in: \(appEnv.name)")
+ 
+        return url
+        
+    }
+    
+    private func getEnvironment() -> AppEnv {
+        #if RELEASE
+            return .prod
+        #elseif DEBUG
+            return .dev
+        #else
+            return .prod
+        #endif
+    }
+}
+
