cb941e95dbb79ba814330ba7ba86b2510b81226f
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-09-24 18:17:50 +0700

    add demo app

---
+++ SDKDemo/SDKDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SDKDemo
+//
+//  Created by Christopher Hicks on 9/24/24.
+//
+
+import SwiftUI
+import StockLiftSDK
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            TestView()
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ SDKDemo/SDKDemo/SDKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDKDemoApp.swift
+//  SDKDemo
+//
+//  Created by Christopher Hicks on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SDKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
