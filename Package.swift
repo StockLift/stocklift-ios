a32d02ce245daba2d9f63967ffe0081d43110cf7
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-09-25 13:09:09 +0700

    config dependencies

--- Package.swift
+++ Package.swift
@@ -5,17 +5,37 @@ import PackageDescription
 
 let package = Package(
     name: "StockLiftSDK",
+    platforms: [
+      // Only add support for iOS 15 and up.
+      .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "StockLiftSDK",
             targets: ["StockLiftSDK"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(
+            name: "LinkKit",
+            url: "https://github.com/plaid/plaid-link-ios",
+            .upToNextMajor(from: "3.0.0")
+          ),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StockLiftSDK"),
+            name: "StockLiftSDK",
+            dependencies: [
+                .product(name: "LinkKit", package: "LinkKit"),
+            ],
+            path: "Sources",
+            resources: [
+                .process("Resources/Colors.xcassets")
+            ]),
+        
         .testTarget(
             name: "StockLiftSDKTests",
             dependencies: ["StockLiftSDK"]
