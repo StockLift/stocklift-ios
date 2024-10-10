4ccee48badc51801a00320d2647595703fc54f7c
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-10-10 15:04:44 +0700

    token

--- SDKDemo/SDKDemo/AppDelegate.swift
+++ SDKDemo/SDKDemo/AppDelegate.swift
@@ -13,7 +13,8 @@ public final class AppDelegate: NSObject, UIApplicationDelegate {
     public func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
-        //MARK: - set your SDK ACCESS TOKEN https://sdk-stocklift.co/docs/
+        //  MARK: - set your SDK ACCESS TOKEN
+        //  https://sdk-stocklift.co/docs/
         StockLiftSDK.accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Im11V0NRRFdkYXlObk1GdEp5a241SlVrZGhpSjIiLCJlbWFpbCI6InRlc3RAdGVzdC5jb20iLCJpYXQiOjE3Mjg0ODU1OTd9.Qo6xYVdBWLW0lQX3SGOkjsblmUgYCy3WTpCKP0W98vw"
         
         return true
