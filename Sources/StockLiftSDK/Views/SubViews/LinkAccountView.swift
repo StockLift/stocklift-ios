75842dee8b5b6d41d7439a5deaf69dc3489cae49
DevboiDesigns <75514458+DevboiDesigns@users.noreply.github.com>
2024-11-21 01:28:27 +0700

    cleanup

--- Sources/StockLiftSDK/Views/SubViews/LinkAccountView.swift
+++ Sources/StockLiftSDK/Views/SubViews/LinkAccountView.swift
@@ -8,20 +8,17 @@
 
 import SwiftUI
 
-
 @available(iOS 14.0, *)
 struct LinkAccountView: View {
     var linkAccountHeader: String = "Add a brokerage account to get a free detailed breakdown of your investments"
-    let plaidError: () -> Void
-    let getPortfolio: () -> Void
-    let foregroundColor: Color
-    let backgroundColor: Color
-    let borderColor: Color
-    let connectSize: CGFloat
-    let font: Font
-    let fontColor: Color
-    
-    @State private var isLoading: PlaidLoadState = .loading
+    var plaidError: () -> Void
+    var getPortfolio: () -> Void
+    var foregroundColor: Color
+    var backgroundColor: Color
+    var borderColor: Color
+    var connectSize: CGFloat
+    var font: Font
+    var fontColor: Color
     
     private var HeaderView: some View {
         if #available(iOS 16.0, *) {
@@ -40,43 +37,22 @@ struct LinkAccountView: View {
     }
     
     var body: some View {
-        Group {
-            switch isLoading {
-            case .loading:
-                ProgressView()
-            case .loaded:
-                // Plaid Link flow
-                OpenLinkButton(getPortfolio: getPortfolio, errorHandler: plaidError) {
-                    VStack(alignment: .center, spacing: 24) {
-                        VStack(spacing: 16) {
-                            HeaderView
-                            Image(systemName: ImageKeys.plusCircleFill)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: connectSize, height: connectSize)
-                                .background(backgroundColor)
-                                .foregroundColor(foregroundColor)
-                                .clipShape(Circle())
-                        }
-                        .padding()
-                        .appBorderOverlay(borderColor: borderColor, backgroundColor: backgroundColor)
-                    }
+        // Plaid Link flow
+        OpenLinkButton(getPortfolio: getPortfolio, errorHandler: plaidError) {
+            VStack(alignment: .center, spacing: 24) {
+                VStack(spacing: 16) {
+                    HeaderView
+                    Image(systemName: ImageKeys.plusCircleFill)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: connectSize, height: connectSize)
+                        .background(backgroundColor)
+                        .foregroundColor(foregroundColor)
+                        .clipShape(Circle())
                 }
-            case .failed:
-                EmptyView()
-                    .onAppear {
-                        print("Error in getting Plaid Link Token. Did you configure your access token?")
-                    }
+                .padding()
+                .appBorderOverlay(borderColor: borderColor, backgroundColor: backgroundColor)
             }
         }
-//        .onAppear { getPlaidLinkUrl() }
     }
-    
-//    private func getPlaidLinkUrl () {
-//        PlaidViewModel.getPlaidLinkToken { isLoading in
-//            DispatchQueue.main.async {
-//                self.isLoading = isLoading
-//            }
-//        }
-//    }
 }
